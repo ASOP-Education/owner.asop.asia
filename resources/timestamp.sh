bf3b0ab2a85f73b1605db36cbe2dad7449dc1e4c
Andrew <andrew0alt2022@gmail.com>
2025-01-05 12:59:38 +0630

    timpstamp.sh

---
+++ resources/timestamp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+current_utc_timestamp=$(date -u "+%Y-%m-%dT%H:%M:%S%z")
+echo "Current timestamp in UTC: $current_utc_timestamp"
+# The following code will get u a time stamp that you can prove in your ownership
